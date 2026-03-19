b40fc4ac9ae100d611ba77ee52af2a52ba3f7942
rahulsd380 <rahulsd380@gmail.com>
2026-03-19 12:19:09 +0600

    Completed navbar

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+import logo from "./images/logo.webp";
+import heroImage from "./images/hero-image.webp";
+import wholeWheatAtta from "./images/whole-wheat-atta.webp";
+import multigrainAtta from "./images/multigrain-atta.webp";
+import gramFlour from "./images/gram-flour.webp";
+import chanaSattu from "./images/chana-sattu.webp";
+
+export const IMAGES = {
+    logo,
+    heroImage,
+    wholeWheatAtta,
+    multigrainAtta,
+    gramFlour,
+    chanaSattu
+}
